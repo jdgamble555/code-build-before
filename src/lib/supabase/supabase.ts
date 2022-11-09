c453a24aa7c22fdaa2c275ceea67b51a3b73bc1f
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-08 20:23:34 -0600

    is local

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,9 +1,9 @@
 import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/supabase-js';
 
-const isEdge = Object.keys(env).length === 0;
+const isLocal = Object.keys(env).length;
 
 export const supabase = createClient(
-    (isEdge ? process.env.PUBLIC_SUPABASE_URL : env.PUBLIC_SUPABASE_URL) as string,
-    (isEdge ? process.env.PUBLIC_SUPABASE_ANON_KEY : env.PUBLIC_SUPABASE_ANON_KEY) as string
+    (isLocal ? env.PUBLIC_SUPABASE_URL : process.env.PUBLIC_SUPABASE_URL) as string,
+    (isLocal ? env.PUBLIC_SUPABASE_ANON_KEY : process.env.PUBLIC_SUPABASE_ANON_KEY) as string
 );
