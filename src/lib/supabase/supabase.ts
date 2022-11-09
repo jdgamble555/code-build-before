18e3b4eb125831cd73be87c2a928e9d68240f545
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-08 20:20:17 -0600

    isedge

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 const isEdge = Object.keys(env).length === 0;
 
 export const supabase = createClient(
-    (isEdge ? env.PUBLIC_SUPABASE_URL : process.env.PUBLIC_SUPABASE_URL) as string,
-    (isEdge ? env.PUBLIC_SUPABASE_ANON_KEY : process.env.PUBLIC_SUPABASE_ANON_KEY) as string
+    (isEdge ? process.env.PUBLIC_SUPABASE_URL : env.PUBLIC_SUPABASE_URL) as string,
+    (isEdge ? process.env.PUBLIC_SUPABASE_ANON_KEY : env.PUBLIC_SUPABASE_ANON_KEY) as string
 );
