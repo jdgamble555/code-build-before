9debc60d58b7c3c2955323f91c4bbd607bfc592a
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-08 20:12:58 -0600

    test

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,9 +1,11 @@
 import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/supabase-js';
 
+const p = Object.keys(env).length === 0 ? process.env : env;
+
 console.log(Object.keys(env).length)
 
 export const supabase = createClient(
-    env.PUBLIC_SUPABASE_URL as string,
-    env.PUBLIC_SUPABASE_ANON_KEY as string
+    p.PUBLIC_SUPABASE_URL as string,
+    p.PUBLIC_SUPABASE_ANON_KEY as string
 );
