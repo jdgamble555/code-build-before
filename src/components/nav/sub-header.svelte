f0a1088f0adeffa49cc486f41abe2ff6ee59cba1
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-11 20:44:21 -0600

    bc

--- src/components/nav/sub-header.svelte
+++ src/components/nav/sub-header.svelte
@@ -6,13 +6,13 @@
 	const crumbs = breadcrumbs().crumbs;
 </script>
 
-<Button color="secondary" on:click={() => goto('/')}>
+<Button color="primary" on:click={() => goto('/')}>
 	<Icon class="material-icons">home</Icon>
 	<Label class="no-bold">Home</Label>
 </Button>
 
 {#each $crumbs as c}
-	<Button color="secondary" on:click={() => goto(c.location)}>
+	<Button color="primary" on:click={() => goto(c.location)}>
 		<Icon class="material-icons">arrow_forward_ios</Icon>
 		<Label class="no-bold">{c.name}</Label>
 	</Button>
