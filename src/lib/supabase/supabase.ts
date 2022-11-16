7517becd311110675cd12d0afb2c193b09bc3c65
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-16 17:55:25 -0600

    key update

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,9 +1,13 @@
-import { env } from '$env/dynamic/public';
+//import { env } from '$env/dynamic/public';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
-const isLocal = Object.keys(env).length;
+/*const isLocal = Object.keys(env).length;
 
 export const supabase = createClient(
     (isLocal ? env.PUBLIC_SUPABASE_URL : process.env.PUBLIC_SUPABASE_URL) as string,
     (isLocal ? env.PUBLIC_SUPABASE_ANON_KEY : process.env.PUBLIC_SUPABASE_ANON_KEY) as string
-);
+);*/
+
+
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
