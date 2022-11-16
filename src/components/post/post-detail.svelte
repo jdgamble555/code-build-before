d34a115484a4b17f05e5fdea99818ebed0c114a4
Jonathan Gamble <jdgamble555@gmail.com>
2022-11-15 20:57:53 -0600

    post detail

--- src/components/post/post-detail.svelte
+++ src/components/post/post-detail.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-	import type { Post } from '$lib/post.model';
-	import Card, { Content, Media, MediaContent } from '@smui/card';
+	import type { Post, Tag } from '$lib/post.model';
+	import Card, { Content } from '@smui/card';
+	import Time from 'svelte-time';
+	import { Icon } from '@smui/button';
+	import { goto } from '$app/navigation';
+	import Chip, { Set, Text } from '@smui/chips';
+	import { add_classes } from 'svelte/internal';
 
 	export let post: Post;
 </script>
@@ -9,9 +14,49 @@
 	{#if post.image}
 		<img class="image" src={post.image} alt={post.title} width="1250" height="650" />
 	{/if}
+	<Content>
+		<a href={'/p/' + post.id + post.slug}>
+			<h2>
+				<span class="blue material-icons card-icon">library_books</span>
+				{post.title}
+			</h2>
+		</a>
 
-	<Content class="centered">
-		<h2>{post.title}</h2>
+		<h6><i>{post.minutes} min read</i></h6>
+		<a href={'/u' + post.author.id + post.author.username}>
+			<div class="ng-author ng-link">
+				{#if post.author?.photoURL}
+					<img
+						class="avatar"
+						width="2.5em"
+						height="2.5em"
+						src={post.author.photoURL}
+						alt={post.author.displayName}
+					/>
+				{:else}
+					<Icon class="material-icons">link</Icon>
+				{/if}
+
+				<p>
+					<small>
+						<b>{post.author.username}</b>
+						{#if post.publishedAt}
+							on <Time timestamp={post.publishedAt} format="dddd, MMMM D, YYYY" />
+						{/if}
+						{#if post.updatedAt}
+							(last modified on <Time timestamp={post.updatedAt} format="dddd, MMMM D, YYYY" />)
+						{/if}
+					</small>
+				</p>
+			</div>
+		</a>
+		{#if post.tags}
+			<Set chips={post.tags} let:chip>
+				<Chip class="grey-tag" {chip} on:click={() => goto('/t/' + chip)}>
+					<Text>{chip}</Text>
+				</Chip>
+			</Set>
+		{/if}
 	</Content>
 </Card>
 <br />
@@ -24,4 +69,22 @@
 		overflow: hidden;
 		height: auto;
 	}
+	.avatar {
+		width: 2.5em !important;
+		height: 2.5em !important;
+		border-radius: 50% !important;
+	}
+	.card-icon {
+		padding-right: 10px;
+		vertical-align: middle;
+		padding-bottom: 5px;
+	}
+	.ng-author {
+		display: flex;
+		align-items: center;
+	}
+	.ng-author img {
+		margin-right: 10px;
+	}
+
 </style>
