b6537a4db0c9601f505b8d93502a7f62aa8fab99
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-10 20:55:22 -0600

    sitemap and feed

---
+++ src/routes/feed/+server.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { RequestHandler } from './$types';
+import { encode } from 'j-supabase';
+import { settings } from '$lib/settings';
+import { supabase } from '$lib/supabase/supabase';
+import { error as sk_error } from '@sveltejs/kit';
+import { Feed } from "feed";
+
+export const GET: RequestHandler = async () => {
+
+    const feed = new Feed({
+        title: settings.meta_title,
+        description: settings.meta_description,
+        id: "https://code.build",
+        link: "https://code.build",
+        language: "en",
+        favicon: "https://code.build/favicon.ico",
+        copyright: "©2022 Code.Build",
+        feedLinks: {
+            atom: "https://code.build/feed"
+        },
+        author: {
+            name: "Jonathan Gamble",
+            link: "https://code.build/u/86qJ3nXSvhBPUbmdaMwUeb/jdgamble555"
+        }
+    });
+
+    // post pages
+    const { data: posts, error } = await supabase.from('posts_hearts_tags').select('*, author(*)').lte('published_at', new Date().toISOString());;
+    if (error) {
+        console.error(error);
+    }
+
+    posts?.forEach(post => {
+        const tags: string[] = JSON.parse(JSON.stringify(post.tags));
+        feed.addItem({
+            title: post.title,
+            id: encode(post.id),
+            link: `https://code.build/p/${encode(post.id)}/${post.slug}`,
+            // description: post.description,
+            content: post.content,
+            date: new Date(post['published_at']),
+            image: post.image,
+            category: tags.map((t) => ({ name: t })) as any,
+            author: [{
+                name: post.author.display_name,
+                link: "https://code.build/u/" + encode(post.author.id) + '/' + post.author.username
+            }]
+        });
+    });
+
+    try {
+
+        return new Response(feed.atom1(), {
+            headers: {
+                "Content-Type": "application/xml"
+            }
+        });
+
+    } catch (e: any) {
+        console.error(e);
+        throw sk_error(500, e);
+    }
+};
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,23 +1,115 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { RequestHandler } from './$types';
+import { encode } from 'j-supabase';
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createGzip } from 'zlib';
+import { settings } from '$lib/settings';
+import { supabase } from '$lib/supabase/supabase';
+import { error } from '@sveltejs/kit';
 
-export const GET: RequestHandler = () => {
-  return new Response(
-    `
-      <?xml version="1.0" encoding="UTF-8" ?>
-      <urlset
-        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="https://www.w3.org/1999/xhtml"
-        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-      >
-        <!-- <url> elements go here -->
-      </urlset>`.trim(),
-    {
-      headers: {
-        'Content-Type': 'application/xml'
+export const GET: RequestHandler = async () => {
+
+  try {
+    const sitemapStream = new SitemapStream({
+      hostname: settings.domain,
+      xmlns: {
+        news: false,
+        xhtml: true,
+        image: true,
+        video: false
+      },
+      xslUrl: 'sitemap.xsl'
+    });
+    const pipeline = sitemapStream.pipe(createGzip());
+
+    let lastmod = new Date(1 - 1 - 2000);
+
+    // todo - separate into different files
+    // todo - put supabase code into separate file
+
+    // post pages
+    const { data, error } = await supabase.from('posts').select('*').lte('published_at', new Date().toISOString());
+    if (error) {
+      console.error(error);
+    }
+    data?.forEach((doc) => {
+      
+      const date = new Date(doc['updated_at'] ?? doc['published_at']);
+      const pid = encode(doc.id);
+
+      // set date for site last updated
+      if (lastmod < date) {
+        lastmod = date;
+      }
+
+      sitemapStream.write({
+        lastmod: date.toISOString(),
+        url: `p/${pid}/${doc.slug}`,
+        img: doc.image
+      });
+
+    });
+
+    // user pages
+    const { data: d3, error: e3 } = await supabase.from('profiles').select('*');
+    if (e3) {
+      console.error(e3);
+    }
+    if (d3) {
+      for (const doc of d3) {
+        const uid = encode(doc.id);
+        const { data: d4, error: e4 } = await supabase.from('posts').select('updated_at').eq('author', doc.id).order('updated_at', { ascending: false }).limit(1);
+        if (e4) {
+          console.error(d4);
+        }
+        const date = d4 && d4.length ? new Date(d4[0]['updated_at']) : new Date(doc['updated_at']);
+        sitemapStream.write({
+          lastmod: date.toISOString(),
+          url: `u/${uid}/${doc.username}`,
+          img: doc['photo_url']
+        });
       }
     }
-  );
+
+    // tag pages
+    const { data: d2, error: e2 } = await supabase.from('tags_latest').select('*');
+    if (e2) {
+      console.error(e2);
+    }
+    if (d2) {
+      for (const doc of d2) {
+
+        const date = new Date(doc['updated_at']);
+
+        sitemapStream.write({
+          lastmod: date.toISOString(),
+          url: `t/${doc.name}`
+        });
+      }
+    }
+
+    // homepage
+    sitemapStream.write({
+      lastmod,
+      url: settings.domain
+    });
+
+    sitemapStream.end();
+
+    const _stream = await streamToPromise(pipeline)
+      .catch((e) => {
+        throw e;
+      });
+
+    return new Response(_stream, {
+      headers: {
+        "Content-Type": "application/xml",
+        "Content-Encoding": "gzip"
+      }
+    });
+
+  } catch (e: any) {
+    console.error(e);
+    throw error(500, e);
+  }
 };
