6793f18c6f924974777ac7459e83ec9f537ae827
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-17 20:20:45 -0600

    test

--- src/routes/p/[id]/[slug]/+layout.ts
+++ src/routes/p/[id]/[slug]/+layout.ts
@@ -3,6 +3,8 @@ import { read_post } from '$lib/database';
 import type { LayoutLoad } from './$types';
 import { get } from 'svelte/store';
 import { postDetail } from '$lib/post-store';
+import { browser } from '$app/environment';
+import type { Optional, Post } from '$lib/post.model';
 
 const { getPostById } = read_post;
 
@@ -15,12 +17,13 @@ export const load: LayoutLoad = async ({ params, setHeaders }) => {
 
     const slug = params.slug;
     const id = params.id;
+    let post: Optional<Post>;
     if (slug && id) {
         // check for pre-loaded post
-        let post = get(postDetail);
-        console.log(post);
+        if (browser) {
+            post = get(postDetail);
+        }
         if (!post) {
-            console.log('getting post');
             // otherwise grab post info
             const { data, error: e } = await getPostById(id);
             if (e) {
