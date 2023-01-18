415cb70c0a0900c2e3e4c5ad7c542df2d8d4600c
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-17 20:54:23 -0600

    null test

--- src/lib/post-store.ts
+++ src/lib/post-store.ts
@@ -2,6 +2,6 @@ import { writable } from "svelte/store";
 import type { Optional, Post } from "./post.model";
 
 export const postList = writable<Post[]>([]);
-export const postDetail = writable<Optional<Post>>(null);
+export const postDetail = writable<Optional<Post>>();
 
 export const currentPage = writable(1);
--- src/routes/p/[id]/[slug]/+layout.ts
+++ src/routes/p/[id]/[slug]/+layout.ts
@@ -21,7 +21,7 @@ export const load = (async ({ params, setHeaders }) => {
     if (slug && id) {
         // check for pre-loaded post
         if (browser) {
-            post = get(postDetail);
+            post = get(postDetail || null);
         }
         if (!post) {
             // otherwise grab post info
