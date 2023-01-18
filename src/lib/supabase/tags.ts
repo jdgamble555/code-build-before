938e6f3be98c58ed2fd68e0651750f5cf3d08599
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-17 19:48:06 -0600

    tags

--- src/lib/supabase/tags.ts
+++ src/lib/supabase/tags.ts
@@ -5,6 +5,6 @@ import { supabase } from "./supabase";
 export const getTags = async (): Promise<{ data: Tag[] | null, error: any }> => {
     const { error, data } = await supabase.from('tag_cloud')
         .select('*')
-        .order('name', { ascending: false });
+        .order('name');
     return { data: data as any, error };
 }
